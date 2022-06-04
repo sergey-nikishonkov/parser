426fc059d903beed9333caf93ffdce27dda3d11d
Sergey <sergej.nikishonkov@mail.ru>
2022-06-04 12:14:24 +0300

    test

--- main_parser.py
+++ main_parser.py
@@ -1,8 +1,8 @@
+import os
+from threading import Thread
 import requests as re
 from bs4 import BeautifulSoup
-from threading import Thread
 from pandas import DataFrame, ExcelWriter
-import os
 
 
 def collect_data(url, headers, category, games):
@@ -21,6 +21,7 @@ def collect_data(url, headers, category, games):
     def page_parser():
         response = re.get(url, headers=headers, params=params)
         soup = BeautifulSoup(response.json()['results_html'], 'html.parser')
+        classes = ("discount_original_price", )
         game_data = [
             (
                 tag.find(class_="tab_item_name").text,
@@ -47,24 +48,24 @@ def collect_data(url, headers, category, games):
 def main():
     url = 'https://store.steampowered.com/contenthub/querypaginated/category/NewReleases/render/'
     headers = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0',
         'Connection': 'keep - alive',
         'ec-ch-ua-platform': 'Windows',
         'Referer': 'https: // store.steampowered.com / category / action /',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36 OPR/86.0.4363.23'
     }
     games = {}
+    types = ['action', 'adventure_and_casual', 'rpg', 'simulation', 'strategy', 'sports_and_racing']
     pool = [
         Thread(target=collect_data, kwargs={'url': url, 'headers': headers, 'category': category, 'games': games}) for
-        category in ['action', 'adventure_and_casual', 'rpg', 'simulation', 'strategy', 'sports_and_racing']
-    ]
+        category in types]
+    
     for thread in pool:
         thread.start()
     for thread in pool:
         thread.join()
 
     for key in games:
-        df = DataFrame(
+        dataframe = DataFrame(
             {
                 'Title': [i[0] for i in games[key]],
                 'Link': [i[1] for i in games[key]],
@@ -76,7 +77,7 @@ def main():
         with ExcelWriter('./games.xlsx',
                          mode='a' if os.path.exists('./games.xlsx') else 'w',
                          if_sheet_exists='overlay') as writer:
-            df.to_excel(writer, sheet_name=key)
+            dataframe.to_excel(writer, sheet_name=key)
 
 if __name__ == '__main__':
     main()
