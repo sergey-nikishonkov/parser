b99d1f503b80868d3e9b490e65e2728d86a37f4e
s.nikishonkov <s.nikishonkov@astondevs.ru>
2023-08-21 23:51:03 +0300

    tets

--- client.py
+++ client.py
@@ -1,1 +1,6 @@
-class Client: pass
+class Client:
+    def train(self): pass
+    def test(self): pass
+    def get_weights(self): pass
+    def get_prediction(self): pass
+    def set_weights(self): pass
